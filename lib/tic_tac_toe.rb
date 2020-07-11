73b8849b1f1af973a2e7b92ba2496556421bda3d
Celina Jackson <celinanjackson@gmail.com>
2020-07-11 22:40:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,5 +12,12 @@ class TicTacToe
     [3,4,5]
     
   ]
+  def display_board
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}}" 
+    puts "-----------"
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}}" 
+    puts "-----------"
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}}" 
+  end
 
 end
